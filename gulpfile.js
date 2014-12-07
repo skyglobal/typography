876bad420f0f7965a963f69f68a58c5f390280e7
Peter Mouland <peter.mouland@gmail.com>
2014-12-07 22:27:10 +0000

    update version dependencies + fix demo page chevrons

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,13 @@ var skyComponentHelper = require('gulp-sky-component-helper')(gulp, pkg);
 var paths = skyComponentHelper.paths;
 var browserSync = require('browser-sync');
 var runSequence = require('run-sequence');
+
+
+gulp.task('copy-icons', function() {
+    return gulp.src(['./bower_components/bskyb-skycons/dist/fonts/*.*'])
+        .pipe(gulp.dest(paths.site['fonts']));
+});
+
+gulp.task('pre-build', function(cb){
+    return runSequence('copy-icons', cb);
+});
