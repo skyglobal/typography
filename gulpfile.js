937fa0972322d1a1c116f9d03eae15b6c9f81182
Peter Mouland <peter.mouland@gmail.com>
2014-12-08 09:31:49 +0000

    removed manual build step with helper update

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,3 @@ var skyComponentHelper = require('gulp-sky-component-helper')(gulp, pkg);
 var paths = skyComponentHelper.paths;
 var browserSync = require('browser-sync');
 var runSequence = require('run-sequence');
-
-
-gulp.task('copy-icons', function() {
-    return gulp.src(['./bower_components/bskyb-skycons/dist/fonts/*.*'])
-        .pipe(gulp.dest(paths.site['fonts']));
-});
-
-gulp.task('pre-build', function(cb){
-    return runSequence('copy-icons', cb);
-});
