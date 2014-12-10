95f2444f56b9b0cef380e6235d10ad6514a27372
Peter Mouland <peter.mouland@gmail.com>
2014-12-10 18:53:24 +0000

    fix config

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
     bower: bower,
     aws:{
         release: true,
-        bucket: process.env.AWS_BUCKET,
+        bucket: process.env.AWS_SKYGLOBAL_BUCKET,
         key: process.env.AWS_ACCESS_KEY_ID,
         secret: process.env.AWS_SECRET_ACCESS_KEY,
         region: process.env.AWS_REGION
