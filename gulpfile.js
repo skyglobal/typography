40267e7c250413aaf8087ac99048d7a48a5e8bf4
Peter Mouland <peter.mouland@gmail.com>
2014-12-23 14:31:02 +0000

    add fonts to aws

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,12 @@ var gulp = require('gulp');
 var componentHelper = require('gulp-component-helper')(gulp);
 var paths = componentHelper.paths;
 var runSequence = require('run-sequence');
+
+gulp.task('copy-fonts', function() {
+    return gulp.src([paths.source['fonts'] + '/*.*'])
+        .pipe(gulp.dest(paths.dist['fonts']));
+});
+
+gulp.task('pre-build', function(cb){
+    return runSequence('copy-fonts', cb);
+});
